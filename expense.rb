efa3fd720c74c173d3735fbb984012b671d9c608
Venkatesh-0304 <156488647+Venkatesh-0304@users.noreply.github.com>
2026-06-03 12:58:56 +0530

    removed testing statements

--- expense.rb
+++ expense.rb
@@ -1,4 +1,3 @@
-require_relative "category"
 class Expense 
   @@expense_id = 0
 
@@ -19,11 +18,3 @@ class Expense
     @spent_on = spent_on
   end
 end
-
-food = Category.new("food")
-e = Expense.new("pizza", 30, food, "3/june/26")
-puts e.title
-puts e.amount
-puts e.category.name
-puts e.spent_on
-puts e.expense_id
