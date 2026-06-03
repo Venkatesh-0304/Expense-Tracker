21adde72bd7f643bf3bc59e9737214714dc8021b
Venkatesh-0304 <156488647+Venkatesh-0304@users.noreply.github.com>
2026-06-03 15:44:50 +0530

    Date object in creating a expense

--- main.rb
+++ main.rb
@@ -1,3 +1,4 @@
+require "date"
 require_relative "expense_not_found_error"
 require_relative "invalid_amount_error"
 require_relative "invalid_category_error"
@@ -7,7 +8,6 @@ require_relative "user"
 require_relative "report_generator"
 
 
-
 u = User.new("bhoomi", "bhoomi@gmail.com")
 
 food = Category.new("food")
@@ -15,12 +15,12 @@ food.add_category
 transport = Category.new("transport")
 transport.add_category
 
-e = Expense.new("pizza", 200, food, "3/june/26")
+e = Expense.new("pizza", 200, food, Date.new(2025, 8, 5))
 # e = Expense.new("pizza", 0, food, "3/june/26")  # raises InvalidAmountError
-e2 = Expense.new("burger", 100, food, "30/may/26")
-e3 = Expense.new("ice cream", 50, food, "29/may/26")
-e4 = Expense.new("coke", 40, food, "28/may/26")
-e5 = Expense.new("chocolate", 50, transport, "25/may/26")
+e2 = Expense.new("burger", 100, food, Date.new(2026, 12, 20))
+e3 = Expense.new("ice cream", 50, food, Date.new(2024, 6, 23))
+e4 = Expense.new("coke", 40, food, Date.new(2025, 8, 5))
+e5 = Expense.new("chocolate", 50, transport, Date.new(2026, 12, 25))
 
 
 u.add_expense(e)
